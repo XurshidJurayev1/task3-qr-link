28c18d15ff3fa3fe2fdaaf9da36819527e0a5726
XurshidJurayev1 <xjurayev99@gmail.com>
2022-10-06 22:47:08 +0500

    111

--- src/Check/index.js
+++ src/Check/index.js
@@ -31,22 +31,25 @@ const Index = () => {
     Username: 'ContractUser', Password: 'X9knh2W6c9SP2H08',
   };
 
+
+  const func = async () => {
+    await api().post('trade/hs/orderscontract/contract', { order: url }, {
+      headers: {
+        Authorization: 'Basic ' + btoa(user.Username + ':' + user.Password),
+      },
+
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
     if (url.length) {
-      const func = async () => {
-        await api().post('trade/hs/orderscontract/contract', { order: url }, {
-          headers: {
-            Authorization: 'Basic ' + btoa(user.Username + ':' + user.Password),
-          },
 
-        })
-          .then(res => {
-            console.log(res);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      };
       func();
     }
   }, []);
@@ -93,228 +96,228 @@ const Index = () => {
 
 
   return (<div className={s.main}>
-      <div className={s.imgContainer}>
-        {/*<div className={s.img} style={{ backgroundImage: `url(${bg})` }} />*/}
-        <Logo />
+    <div className={s.imgContainer}>
+      {/*<div className={s.img} style={{ backgroundImage: `url(${bg})` }} />*/}
+      <Logo />
+    </div>
+    <div className={s.titleDiv}>
+      <h3 className={s.title}>Договор купли продажи № 764803 от 17.08.2022
+      </h3>
+    </div>
+    <div className={s.container}>
+      <div className={s.date}>
+        <p>
+          г. Ташкент
+
+        </p>
+        <p>
+          17.08.2022
+
+        </p>
       </div>
-      <div className={s.titleDiv}>
-        <h3 className={s.title}>Договор купли продажи № 764803 от 17.08.2022
-        </h3>
+
+      <div className={s.select}>
+        <div className={s.selectHeader} onClick={() => setSelect1(!select1)}>
+          <div className={s.iconSelect} style={select1 ? { transform: 'rotate(-180deg)' } : null}>
+            <AiOutlineDown />
+          </div>
+          <div className={s.selectTitle}>
+            <p>
+              Продавец:
+            </p>
+            <p>
+              <span>YaTT AKBAROV M.A.</span>
+            </p>
+          </div>
+        </div>
+        <div className={s.selectContent} style={select1 ? { height: '195px' } : { height: '0px' }}>
+          <div className={s.selectContentEl}>
+            <p>
+              Город:
+            </p>
+            <p>
+              Ташкент
+            </p>
+          </div>
+          <div className={s.selectContentEl}>
+            <p>
+              Адрес:
+            </p>
+            <p>
+            </p>
+          </div>
+          <div className={s.selectContentEl}>
+            <p>
+              ИНН:
+            </p>
+            <p>
+              517029982
+            </p>
+          </div>
+          <div className={s.selectContentEl}>
+            <p>
+              Тел:
+            </p>
+            <p>
+              555036996
+            </p>
+          </div>
+        </div>
       </div>
-      <div className={s.container}>
-        <div className={s.date}>
+
+      <div className={s.select}>
+        <div className={s.selectHeader} onClick={() => setSelect2(!select2)}>
+          <div className={s.iconSelect} style={select2 ? { transform: 'rotate(-180deg)' } : null}>
+            <AiOutlineDown />
+          </div>
+          <div className={s.selectTitle}>
+            <p>
+              Покупатель:
+            </p>
+            <p>
+              <span>Rashid Jahonov Pirimqulovich</span>
+            </p>
+          </div>
+        </div>
+        <div className={s.selectContent} style={select2 ? { height: '150px' } : { height: '0px' }}>
+          <div className={s.selectContentEl}>
+            <p>
+              Город:
+            </p>
+            <p>
+              Карши
+            </p>
+          </div>
+          <div className={s.selectContentEl}>
+            <p>
+              Адрес:
+            </p>
+            <p>
+            </p>
+          </div>
+          <div className={s.selectContentEl}>
+            <p>
+              Тел:
+            </p>
+            <p>
+              998996682000
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <p className={s.text}>
+        Продавец передает в собственность Покупателя товар (наименование, вид, характеристики товара), а Покупатель
+        обязуется принять Товар и уплатить за него цену в размере и в порядке, предусмотренных Договором.
+      </p>
+
+      <div id="blank" className={s.table}>
+        <p className={s.tableTitle}>
+          Информация о товаре
+        </p>
+        <div className={s.tableEl} style={{ borderTop: '0px ! important' }}>
           <p>
-            г. Ташкент
-
+            Наименование товара
           </p>
+          <span className={s.border} />
           <p>
-            17.08.2022
-
+            <span>PowerMan low</span>
+          </p>
+        </div>
+        <div className={s.tableEl}>
+          <p>
+            Количество
+          </p>
+          <span className={s.border} />
+          <p>
+            <span> 1</span>
+          </p>
+        </div>
+        <div className={s.tableEl}>
+          <p>
+            Цена
+          </p>
+          <span className={s.border} />
+          <p>
+            <span> 297 000</span>
+          </p>
+        </div>
+        <div className={s.tableEl}>
+          <p>
+            Сумма
+          </p>
+          <span className={s.border} />
+          <p>
+            <span> 297 000</span>
+          </p>
+        </div>
+        <div className={s.tableEl}>
+          <p>
+            Сумма доставки
+          </p>
+          <span className={s.border} />
+          <p>
+            <span> 50 000</span>
           </p>
         </div>
 
-        <div className={s.select}>
-          <div className={s.selectHeader} onClick={() => setSelect1(!select1)}>
-            <div className={s.iconSelect} style={select1 ? { transform: 'rotate(-180deg)' } : null}>
-              <AiOutlineDown />
-            </div>
-            <div className={s.selectTitle}>
-              <p>
-                Продавец:
-              </p>
-              <p>
-                <span>YaTT AKBAROV M.A.</span>
-              </p>
-            </div>
-          </div>
-          <div className={s.selectContent} style={select1 ? { height: '195px' } : { height: '0px' }}>
-            <div className={s.selectContentEl}>
-              <p>
-                Город:
-              </p>
-              <p>
-                Ташкент
-              </p>
-            </div>
-            <div className={s.selectContentEl}>
-              <p>
-                Адрес:
-              </p>
-              <p>
-              </p>
-            </div>
-            <div className={s.selectContentEl}>
-              <p>
-                ИНН:
-              </p>
-              <p>
-                517029982
-              </p>
-            </div>
-            <div className={s.selectContentEl}>
-              <p>
-                Тел:
-              </p>
-              <p>
-                555036996
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className={s.select}>
-          <div className={s.selectHeader} onClick={() => setSelect2(!select2)}>
-            <div className={s.iconSelect} style={select2 ? { transform: 'rotate(-180deg)' } : null}>
-              <AiOutlineDown />
-            </div>
-            <div className={s.selectTitle}>
-              <p>
-                Покупатель:
-              </p>
-              <p>
-                <span>Rashid Jahonov Pirimqulovich</span>
-              </p>
-            </div>
-          </div>
-          <div className={s.selectContent} style={select2 ? { height: '150px' } : { height: '0px' }}>
-            <div className={s.selectContentEl}>
-              <p>
-                Город:
-              </p>
-              <p>
-                Карши
-              </p>
-            </div>
-            <div className={s.selectContentEl}>
-              <p>
-                Адрес:
-              </p>
-              <p>
-              </p>
-            </div>
-            <div className={s.selectContentEl}>
-              <p>
-                Тел:
-              </p>
-              <p>
-                998996682000
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <p className={s.text}>
-          Продавец передает в собственность Покупателя товар (наименование, вид, характеристики товара), а Покупатель
-          обязуется принять Товар и уплатить за него цену в размере и в порядке, предусмотренных Договором.
-        </p>
-
-        <div id="blank" className={s.table}>
-          <p className={s.tableTitle}>
-            Информация о товаре
+        <div className={s.tableEl}>
+          <p>
+            ИТОГО:
           </p>
-          <div className={s.tableEl} style={{ borderTop: '0px ! important' }}>
-            <p>
-              Наименование товара
-            </p>
-            <span className={s.border} />
-            <p>
-              <span>PowerMan low</span>
-            </p>
-          </div>
-          <div className={s.tableEl}>
-            <p>
-              Количество
-            </p>
-            <span className={s.border} />
-            <p>
-              <span> 1</span>
-            </p>
-          </div>
-          <div className={s.tableEl}>
-            <p>
-              Цена
-            </p>
-            <span className={s.border} />
-            <p>
-              <span> 297 000</span>
-            </p>
-          </div>
-          <div className={s.tableEl}>
-            <p>
-              Сумма
-            </p>
-            <span className={s.border} />
-            <p>
-              <span> 297 000</span>
-            </p>
-          </div>
-          <div className={s.tableEl}>
-            <p>
-              Сумма доставки
-            </p>
-            <span className={s.border} />
-            <p>
-              <span> 50 000</span>
-            </p>
-          </div>
-
-          <div className={s.tableEl}>
-            <p>
-              ИТОГО:
-            </p>
-            <span className={s.border} />
-            <p>
-              <span> 347 000</span>
-            </p>
-          </div>
-
-        </div>
-
-        <p className={s.text}>Продавец обязан передать Покупателю Товар надлежащего качества и в надлежащей упаковке в
-          порядке и в сроки, предусмотренные настоящим Договором, передать Товар свободным от прав третьих лиц.
-        </p>
-        <p className={s.text}>
-          Покупатель обязан принять Товар от Продавца в порядке и в сроки, предусмотренные настоящим Договором, оплатить
-          Товар в порядке и в сроки, предусмотренные настоящим Договором.
-        </p>
-        <p className={s.text}>Цена Товара, передаваемого по настоящему Договору, составляет: 297 000 (двести
-          девяносто семь тысяч) сум.</p>
-        <p className={s.text}>
-          Датой оплаты считается дата передачи Покупателем наличных денежных средств Продавцу либо дата поступления
-          денежных средств на счет Продавца.
-        </p>
-        <p className={s.text}>
-          Просим проверить целостность упаковки при получении товара.
-        </p>
-        <p className={s.text}>
-          Благодарим за покупку!
-        </p>
-
-        <div className={s.divCertificate}>
-          <p className={s.certificate} onClick={() => setModal(true)}>
-            сертификаты
-
-          </p>
-
-          <p className={s.certificate} onClick={downloadBlank}>
-            Скачать документов
+          <span className={s.border} />
+          <p>
+            <span> 347 000</span>
           </p>
         </div>
-
 
       </div>
-      <Modal
-        isOpen={modal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <div className={s.close} onClick={() => setModal(false)}>
-          <AiFillCloseSquare />
-        </div>
-        <div className={s.modalImg} style={{ backgroundImage: `url(${img})` }} id="certificate" />
-      </Modal>
-    </div>);
+
+      <p className={s.text}>Продавец обязан передать Покупателю Товар надлежащего качества и в надлежащей упаковке в
+        порядке и в сроки, предусмотренные настоящим Договором, передать Товар свободным от прав третьих лиц.
+      </p>
+      <p className={s.text}>
+        Покупатель обязан принять Товар от Продавца в порядке и в сроки, предусмотренные настоящим Договором, оплатить
+        Товар в порядке и в сроки, предусмотренные настоящим Договором.
+      </p>
+      <p className={s.text}>Цена Товара, передаваемого по настоящему Договору, составляет: 297 000 (двести
+        девяносто семь тысяч) сум.</p>
+      <p className={s.text}>
+        Датой оплаты считается дата передачи Покупателем наличных денежных средств Продавцу либо дата поступления
+        денежных средств на счет Продавца.
+      </p>
+      <p className={s.text}>
+        Просим проверить целостность упаковки при получении товара.
+      </p>
+      <p className={s.text}>
+        Благодарим за покупку!
+      </p>
+
+      <div className={s.divCertificate}>
+        <p className={s.certificate} onClick={() => setModal(true)}>
+          сертификаты
+
+        </p>
+
+        <p className={s.certificate} onClick={downloadBlank}>
+          Скачать документов
+        </p>
+      </div>
+
+
+    </div>
+    <Modal
+      isOpen={modal}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <div className={s.close} onClick={() => setModal(false)}>
+        <AiFillCloseSquare />
+      </div>
+      <div className={s.modalImg} style={{ backgroundImage: `url(${img})` }} id="certificate" />
+    </Modal>
+  </div>);
 };
 
 export default Index;
